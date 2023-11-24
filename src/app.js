15cf9d21166133c2cb68b3c8e1e47df7b5470a0b
Khoa Tran” <khoab1710138@student.ctu.edu.vn>
2023-11-24 19:10:54 +0700

    Implement an error handling middlewares

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 
 const app = express();
 const contactsRouter = require('./routes/contacts.router');
+const { 
+resourceNotFound, 
+handleError 
+} = require('./controllers/errors.controller'); 
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +16,9 @@ res.json({ message: 'Welcome to contactbook application.' });
 }); 
 
 app.use('/api/contacts', contactsRouter);
+// Handle 404 response 
+app.use(resourceNotFound); 
+// Define error-handling middleware last 
+app.use(handleError); 
 
 module.exports = app;
